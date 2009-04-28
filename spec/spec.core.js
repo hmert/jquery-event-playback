6312d074fe442c10edd66f32b8479227b1042e5e
visionmedia <tj@vision-media.ca>
2009-04-28 10:00:54 -0700

    Misc refactoring

--- spec/spec.core.js
+++ spec/spec.core.js
@@ -1,4 +1,13 @@
 
+$(function(){
+  $.recordEvents({ duration: 2000, finished : function(){
+    console.log('finished recording');
+    $.playbackEvents(this, { duration : 3000, finished : function() {
+      console.log('finished playback');
+    }})
+  }})
+})
+
 describe 'jQuery'
   describe '.recordEvents()'
     it 'should should responsd to recordEvents'
