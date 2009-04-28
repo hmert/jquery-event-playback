9467c7379d8684403867730d15e6eeff5ec0e384
visionmedia <tj@vision-media.ca>
2009-04-28 12:15:12 -0700

    Started constructing demo

---
+++ demo/demo.js
@@ -0,0 +1,43 @@
+
+$(function(){
+  var step = 0
+  $('.example').hide()
+  $('input[type=button]').click(function() {
+    switch (++step) {
+      case 1:
+        $('.e1').show()
+        $('.main').slideDown('slow')
+        $(this).attr('value', 'Continue')
+        break
+        
+      case 2:
+        $('.e1').slideUp('slow')
+        $('.e2').slideDown('slow')
+        $(this).attr('value', 'Continue.')
+        break
+        
+      case 3:
+        $('.e2').slideUp('slow')
+        $('.e3').slideDown('slow')
+        $(this).attr('value', 'Continue..')
+        break
+
+      case 4:
+        $('.e3').slideUp('slow')
+        $('.e4').slideDown('slow')
+        $(this).attr('value', 'Continue...')
+        break
+        
+      case 5:
+        $('.e4').hide('slow')
+        $('.main').slideUp('slow')
+        $(this).attr('value', 'Start Demo')
+        step = 0
+    }
+  })
+  
+  $.recordEvents({ duration: 15000, finished : function(){
+    $.playbackEvents(this, { interval : 20, finished : function() {
+    }})
+  }})
+})
