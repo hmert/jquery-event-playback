c7dbfd8091c83fb3a0cb3077b7f223e10975e2e9
visionmedia <tj@vision-media.ca>
2009-04-29 11:11:58 -0700

    Added delay option

--- demo/demo.js
+++ demo/demo.js
@@ -11,17 +11,15 @@ $(function(){
   $('#start').click(function(){
     $('#log').val('')
     $('#export').val('')
-    setTimeout(function(){
-      duration = parseInt($('#seconds').val()) * 1000
-      log('Started recording: ' + duration + 'ms')
-      $.recordEvents({ duration: duration, finished : function(){
-        $('#export').val(this.export())
-        log('Finished recording')
-        $.playbackEvents(this, { interval : 25, finished : function() {
-          log('Playback finished')
-        }})
+    duration = parseInt($('#seconds').val()) * 1000
+    log('Started recording: ' + duration + 'ms')
+    $.recordEvents({ duration: duration, finished : function(){
+      $('#export').val(this.export())
+      log('Finished recording')
+      $.playbackEvents(this, { interval : 25, finished : function() {
+        log('Playback finished')
       }})
-    }, 1000)
+    }})
   })
   
 })
