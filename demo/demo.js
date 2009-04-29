9100a8e8781a2bf9fdb432f6d039f116821b379e
visionmedia <tj@vision-media.ca>
2009-04-29 11:08:51 -0700

    Added log to demo

--- demo/demo.js
+++ demo/demo.js
@@ -4,14 +4,24 @@ $(function(){
     $(this).attr('value', $(this).attr('value') + '.')
   })
   
+  log = function(message) {
+    $('#log').val($('#log').val() + "\n" + message)
+  }
+  
   $('#start').click(function(){
-    duration = parseInt($('#seconds').val()) * 1000
-    $.recordEvents({ duration: duration, finished : function(){
-      $('#export').val(this.export())
-      $.playbackEvents(this, { interval : 25, finished : function() {
-        
+    $('#log').val('')
+    $('#export').val('')
+    setTimeout(function(){
+      duration = parseInt($('#seconds').val()) * 1000
+      log('Started recording: ' + duration + 'ms')
+      $.recordEvents({ duration: duration, finished : function(){
+        $('#export').val(this.export())
+        log('Finished recording')
+        $.playbackEvents(this, { interval : 25, finished : function() {
+          log('Playback finished')
+        }})
       }})
-    }})
+    }, 1000)
   })
   
 })
