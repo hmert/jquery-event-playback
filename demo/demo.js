b6db15eb32001af609347dc388f6df9beb7e69dc
visionmedia <tj@vision-media.ca>
2009-04-29 10:18:48 -0700

    Better demo

--- demo/demo.js
+++ demo/demo.js
@@ -4,12 +4,14 @@ $(function(){
     $(this).attr('value', $(this).attr('value') + '.')
   })
   
-  $.recordEvents({ duration: 5000, finished : function(){
-    console.log('Recording finished');
-    $('#export').val(this.export())
-    $.playbackEvents(this, { interval : 25, finished : function() {
-      console.log('Playback finished');
+  $('#start').click(function(){
+    duration = parseInt($('#seconds').val()) * 1000
+    $.recordEvents({ duration: duration, finished : function(){
+      $('#export').val(this.export())
+      $.playbackEvents(this, { interval : 25, finished : function() {
+        
+      }})
     }})
-  }})
+  })
   
 })
